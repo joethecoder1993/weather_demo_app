fb0978795b6ff8d4981e5f4aaf680c49216bdff5
Joseph <segintendo@yahoo.com>
2023-06-22 15:33:07 -0400

    finished the app although there are a few bugs where the city parameter is not changing in the HTML code. I hope to address this soon.

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-//stay away from my api key. No touchie >:(
+//stay away from my ap
 
 const weatherData = async (city) => {
     const request = await fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey)
         .then((request) => request.json())
         .then((response) => {
-            console.log("Temperature: " + response.main.temp)
-            console.log("Pressure: " + response.main.pressure)
-            console.log("Humidity: " + response.main.humidity)
+            document.querySelector('#city').innerHTML = "Weather in " + city
+            document.querySelector('#temperature').innerHTML = response.main.temp + " °F"
+            document.querySelector('#icon').src = "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png"
+            document.querySelector('#sky').innerHTML = response.weather[0].description
+            document.querySelector('#humidity').innerHTML = "Humidity: " + response.main.humidity + "%"
+            document.querySelector('#wind-speed').innerHTML = "Wind Speed: " + response.wind.speed + " mph"
+
         })
         .then((data) => {return data})
     //const response = request.json()
     //console.log(response)
 }
 
-function funcCall(c) {
-    weatherData(c)
-}
+document.addEventListener('click', (e) => {
+    if(e.target.id === "button") {
+        //console.log(document.getElementById('input-text').value)
+        console.log(document.getElementById('input-text').value)
+        var url = 'https://source.unsplash.com/1600x900/?' + document.getElementById('input-text').value
+        document.querySelector('.showcase').style.backgroundImage = "url(" + url + ")"
+        weatherData(document.getElementById('input-text').value)
+    }
+})
+
+document.querySelector('.search-bar').addEventListener('keyup', e => {
+    if(e.key === "Enter") {
+        //console.log(document.getElementById('input-text').value)
+        console.log(document.getElementById('input-text').value)
+        var url = 'https://source.unsplash.com/1600x900/?' + document.getElementById('input-text').value
+        document.querySelector('.showcase').style.backgroundImage = "url(" + url + ")"
+        weatherData(document.getElementById('input-text').value)
+    }
+})
